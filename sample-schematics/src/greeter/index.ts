f79d5d6fc62ebefd749563caf7c1341f4eb9aca7
TWulfZ <andyvelezespejo@gmail.com>
2025-11-04 21:39:51 -0500

    generating schemas with parameters (properties)

--- sample-schematics/src/greeter/index.ts
+++ sample-schematics/src/greeter/index.ts
@@ -1,11 +1,17 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Schema } from './schema';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function greeter(_options: any): Rule {
+export function greeter(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    tree.create('hello.js', `console.log('Hello User')`);
+    let name = _options.name;
+
+    if(tree.exists('hello.js)')) {
+      tree.delete('hello.js');
+    }
+    tree.create('hello.js', `console.log('Hello ${name}!');`);
     return tree;
   };
 }

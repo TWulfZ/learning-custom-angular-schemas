68988ccf3a391fd1c011b0d0e2ad9bf1107f5980
TWulfZ <andyvelezespejo@gmail.com>
2025-11-05 16:43:17 -0500

    Integrating schematics with Angular

--- sample-schematics/src/http-resource/index.ts
+++ sample-schematics/src/http-resource/index.ts
@@ -1,20 +1,52 @@
-import { apply, mergeWith, Rule, SchematicContext, strings, template, Tree, url } from '@angular-devkit/schematics';
-
+import {
+  apply,
+  chain,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  strings,
+  template,
+  Tree,
+  url,
+} from "@angular-devkit/schematics";
+import { createDefaultPath, getWorkspace } from "@schematics/angular/utility/workspace";
+import { parseName } from "@schematics/angular/utility/parse-name";
+import { Schema } from "./schema";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function httpResource(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    const sourceTemplate = url('./files');
+export function httpResource(_options: Schema): Rule {
+  return async (tree: Tree, _context: SchematicContext) => {
+    const workspace = await getWorkspace(tree);
+    if (!_options.project) {
+      _options.project = workspace.projects.keys().next().value;
+    }
 
-    const parameterizedTemplate = apply(sourceTemplate, [
+    const project = workspace.projects.get(_options.project);
+    if (!project) {
+      throw new SchematicsException(`Invalid project name: ${_options.project}`);
+    }
+
+    if (_options.path === undefined) {
+      _options.path = await createDefaultPath(tree, _options.project);
+    }
+
+    const parsePath = parseName(_options.path, _options.name);
+    _options.name = parsePath.name;
+    _options.path = parsePath.path;
+
+    const sourceTemplate = url("./files");
+    const sourceParametirizedTemplate = apply(sourceTemplate, [
       template({
         ..._options,
-        ...strings
-      })
+        ...strings,
+      }),
+      move(parsePath.path),
     ]);
-    
-    tree = mergeWith(parameterizedTemplate)(tree, _context) as Tree;
-    return tree;
+
+    return chain([mergeWith(sourceParametirizedTemplate, MergeStrategy.Overwrite)]);
   };
 }
